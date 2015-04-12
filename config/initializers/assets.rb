dbdc0ece1910fb587478f7177bfcb8720b1a4355
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-11 18:12:02 -0700

    fixing issues with css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-Rails.application.config.assets.precompile += %w(.css)
+Rails.application.config.assets.precompile += %w( z_products.css )
