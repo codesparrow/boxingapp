9f0b6feb91f50717e97e16437905d769885254c7
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-12 18:03:51 -0700

    trying to fix issues with layout in heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,7 @@ class ProductsController < ApplicationController
   # GET /products/1/edit
   def edit
   end
+
   # POST /products
   # POST /products.json
   def create
@@ -68,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :colour)
+      params.require(:product).permit(:name, :description, :image_url)
     end
 end
