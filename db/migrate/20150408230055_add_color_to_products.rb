85673912fea8244d4f2a969215432489d8da6fda
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-13 04:06:04 -0700

    deleting old files

--- db/migrate/20150408230055_add_color_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddColorToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :colour, :string
-  end
-end
--- db/migrate/20150410220521_add_attach_paperclip.rb
+++
@@ -1,4 +0,0 @@
-class AddAttachPaperclip < ActiveRecord::Migration
-  def change
-  end
-end
