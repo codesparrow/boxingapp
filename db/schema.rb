98e42afd31236bda0cea77963361dc94d39e0033
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-15 04:35:04 -0700

    deleting files due to error

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413232731) do
-
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "colour"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
