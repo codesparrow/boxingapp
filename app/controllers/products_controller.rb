4f5d0f7db4e4b499442901ea1426e6f9e0a83d87
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-18 11:20:31 -0700

    adding navbar, carousel, layouts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.limit(3)
   end
 
   # GET /products/1
@@ -72,3 +72,4 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:name, :description, :image_url, :colour, :price)
     end
 end
+
