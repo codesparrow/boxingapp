7ff5f8a0829932a64e433ef8e977318307ccc880
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-27 03:34:19 -0700

    fixing css in heroku

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
   belongs_to :product
-=======
->>>>>>> master
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,8 @@
 class Product < ActiveRecord::Base
  has_many :orders
  has_many :comments
-<<<<<<< HEAD
-=======
 
  def average_rating
   comments.average(:rating).to_f
  end
->>>>>>> master
 end
