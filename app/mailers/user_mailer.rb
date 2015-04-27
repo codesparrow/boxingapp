deaebd975409db2688c93311eff8c068fc312718
Juan Pineiro <pineiro_juan@hotmail.com>
2015-04-27 15:02:15 -0700

    modified login portion and automatic email when sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
+  def welcome_email(user)
+   @user = user
+   @url = "http://thesweetscience.herokuapp.com"
+   mail(:to => user.email, :subject => "Thanks for joining!")
+  end
+
   def contact_form(email, name, message)
    @message = message 
    mail(:from => email,
