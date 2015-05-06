e4524360d7a8dc184ce2827038096b9c753f7c62
Juan Pineiro <pineiro_juan@hotmail.com>
2015-05-06 03:45:58 -0700

    added stripe checkout

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,14 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_Jj92vquWFKkC63dgq8ZdPLcz',
-  :secret_key      => 'sk_test_NXawscEfrnE8g0S93jqtQauV'
-}
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_Jj92vquWFKkC63dgq8ZdPLcz',
+    :secret_key      => 'sk_test_NXawscEfrnE8g0S93jqtQauV'
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
