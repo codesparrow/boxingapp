fedaa33f75be9d2581a475bbde9759976cd2d855
Juan Pineiro <pineiro_juan@hotmail.com>
2015-05-10 03:39:17 -0700

    fixing stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,15 +2,13 @@ class PaymentsController < ApplicationController
   def create
     token = params[:stripeToken]
     product = Product.find (params[:product_id])
-    @product_id = @product.price
-
   # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        :amount => @amount, # amount in cents, again
+        :amount => product.price_in_cents, # amount in cents, again
         :currency => "usd",
         :source => token,
-        :description => params[:stripeEmail]
+        :description => product.name
     )
     order = Order.create(product: product, stripe_id: charge.id, stripe_balance_transaction: charge.balance_transaction, total: charge.amount)
     redirect_to order_url(order), notice: "You order has been processed"
